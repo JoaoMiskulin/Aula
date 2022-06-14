7218877c7b587d61b6b9e719682d6acda475cd41
JoaoMiskulin <chefmisku@gmail.com>
2022-06-14 19:10:57 -0300

    alteracao 2 adicao de texto

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -3,6 +3,7 @@ public class Exemplo {
 	
 	public static void sayHello() {
 		System.out.println("Olá mundo!");
+		System.out.println("Vamo que vamo!!");
 	}
 	
 	public static void main(String[] args) {
